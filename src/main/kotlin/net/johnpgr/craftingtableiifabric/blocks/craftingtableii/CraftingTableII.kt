142a3b2b567a7955a1feada16d981c4035215109
thejonyboybr <jp.greidinger@hotmail.com>
2024-02-10 19:53:58 -0300

    (Chore) Update fabric.mod.json add

--- src/main/kotlin/net/johnpgr/craftingtableiifabric/blocks/craftingtableii/CraftingTableII.kt
+++ src/main/kotlin/net/johnpgr/craftingtableiifabric/blocks/craftingtableii/CraftingTableII.kt
@@ -19,7 +19,6 @@ import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
 import net.minecraft.util.shape.VoxelShape
-import net.minecraft.world.BlockRenderView
 import net.minecraft.world.BlockView
 import net.minecraft.world.World
 
